5613ac2f3e252a72be473b3f36b375ece3b030e9
Walid Soussi <Etu_SOUSSI.Walid@emsi-edu.ma>
2021-03-31 21:17:41 +0100

    fix hfe

--- 0x01-ssh/0-use_a_private_key.bash
+++ 0x01-ssh/0-use_a_private_key.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-# Description : this script connect to server with ubuntu user
+# Description : this script connect to server with ubuntu  user
 ssh -i ~/.ssh/holberton ubuntu@35.196.138.224
