12b2ce515ddaa06eef371f83cdb836d8938e62c8
Walid Soussi <Etu_SOUSSI.Walid@emsi-edu.ma>
2021-03-31 21:00:33 +0100

    fix ssh create key pair

--- 0x01-ssh/0-use_a_private_key.bash
+++ 0x01-ssh/0-use_a_private_key.bash
@@ -1,3 +1,3 @@
-#!/user/bin/env bash
+#!/usr/bin/env bash
 # Description : this script connect to server with ubuntu user
 ssh -i ~/.ssh/holberton ubuntu@35.196.138.224
--- 0x01-ssh/1-create_ssh_key_pair.bash
+++ 0x01-ssh/1-create_ssh_key_pair.bash
@@ -1,3 +1,3 @@
-#!/user/bin/env bash
+#!/usr/bin/env bash
 # Description : this bash script create a pear key (private and public)
 ssh-keygen  -b 4096 -f holberton -q -N betty
